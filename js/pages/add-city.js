cf557a3e4dea7d12f9f2d42c85a7c2abe26454f6
Sofia Mazzurco <mazzurquito582@gmail.com>
2022-10-24 17:37:40 -0300

    avance primera parte js

---
+++ js/pages/add-city.js
@@ -0,0 +1,24 @@
+   function addNewCityToLocalStorage() {
+    let newCity = document.getElementById('agregarCiudad').value;
+    let cities = getCitiesFromLocalStorage();
+    if(cities.indexOf(newCity)>=0){
+        document.getElementById("verde").style.display='none';
+        document.getElementById("amarillo").style.display='block';
+    }
+    else {
+        cities.push(newCity);
+        document.getElementById("verde").style.display='block';
+        document.getElementById("amarillo").style.display='none';
+    }
+    localStorage.setItem("CITIES", JSON.stringify(cities));
+}
+
+function getCitiesFromLocalStorage() {
+    let cities = localStorage.getItem("CITIES");
+    if (cities) {
+        cities = JSON.parse(cities);
+    } else {
+        cities = [];
+    }
+    return cities;
+}
