553058a650402b200eb609f9eedfe796ea1163a9
Sofia Mazzurco <mazzurquito582@gmail.com>
2022-11-07 08:41:29 -0300

    avance final

--- js/pages/add-city.js
+++ js/pages/add-city.js
@@ -1,12 +1,43 @@
-function addNewCityToLocalStorage() {
-    let newCity = document.getElementById('agregarCiudad').value;
+var datos
+var ciudadseleccionada
+var res
+
+const addNewCityToLocalStorage = async () => {
+
     let cities = getCitiesFromLocalStorage();
-    if(cities.indexOf(newCity)>=0){
+
+    try{
+        ciudadseleccionada = document.getElementById("agregarCiudad").value;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudadseleccionada}&appid=ed8f7775dc254d70a99c257145d2037e&units=metric&lang=es`
+        res = await fetch(url);
+
+        if(res.ok){
+            datos = await res.json();
+            console.log(datos)
+            
+        }else{
+                console.log(res.status);
+            }
+            
+        }catch(err){
+        console.log(err)
+    }
+
+
+    let newCity = document.getElementById('agregarCiudad').value;
+    if(res.status == "404" || res.status == "400"){
+        document.getElementById("rojo").style.display='block';
+        document.getElementById("verde").style.display='none';
+        document.getElementById("amarillo").style.display='none';
+    }
+    else if (cities.indexOf(newCity)>=0){
+        document.getElementById("rojo").style.display='none';
         document.getElementById("verde").style.display='none';
         document.getElementById("amarillo").style.display='block';
     }
     else {
         cities.push(newCity);
+        document.getElementById("rojo").style.display='none';
         document.getElementById("verde").style.display='block';
         document.getElementById("amarillo").style.display='none';
     }
@@ -22,3 +53,4 @@ function getCitiesFromLocalStorage() {
     }
     return cities;
 }
+
--- js/pages/index.js
+++ js/pages/index.js
@@ -9,15 +9,51 @@ function getCitiesFromLocalStorage() {
 }
 
 function opcionesSelect() {
-    const Ciudad = getCitiesFromLocalStorage();
-    /*Ciudades.map((Ciudad)=> return (<option value={Ciudad}>{Ciudad}</option>));*/
+    const Ciudades = getCitiesFromLocalStorage();
+    Ciudades.map(function(Ciudad){
+        document.write('<option value="'+Ciudad+'">'+Ciudad+'</option>');
+    })
 
-    for(var i = 0; i < Ciudad.length; i++) {
-        var select = document.createElement("select");
-        news.appendChild(select);
-        var option = document.createElement("option");
-        option.value= Ciudad[i];
-        option.innerHTML = Ciudad[i];
-        news.appendChild(option);
 }
-}
+
+var datos
+var ciudadseleccionada
+var res
+
+const llamarDatos = async () => {
+
+    try{
+        ciudadseleccionada = document.getElementById("desplegableCiudades").value;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudadseleccionada}&appid=ed8f7775dc254d70a99c257145d2037e&units=metric&lang=es`
+        res = await fetch(url);
+
+        if(res.ok){
+            datos = await res.json();
+            console.log(datos)
+            
+        }else{
+                console.log(res.status);
+            }
+            
+        }catch(err){
+        console.log(err)
+    }
+
+    if (res.status == "404"){
+        console.log("da error")
+    }
+    else{
+        
+
+        divcard.innerHTML = 
+        `<h3>${ciudadseleccionada}, ${datos.sys.country}</h3>
+        <h3><img src="http://openweathermap.org/img/wn/${datos.weather[0].icon}@2x.png"></h3>
+        <p>Temperatura: ${datos.main.temp}°</p>
+        <p>Sensación térmica: ${datos.main.feels_like}°</p>
+        <p>Humedad: ${datos.main.humidity}%</p>
+        <p>Velocidad del viento: ${datos.wind.speed}km/h</p>
+        <p>Presión: ${datos.main.pressure} p</p>`
+        document.getElementById("divcard").style.display = "block";
+    }
+
+};
